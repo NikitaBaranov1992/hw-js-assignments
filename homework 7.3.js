4dd5ea0c81ea213ad2bef854cd4ac5b631bc56c6
NikitaBaranov1992 <2486010@mail.ru>
2023-04-23 15:20:32 +0500

    homework 7.3

---
+++ homework 7.3.js
@@ -0,0 +1,35 @@
+class sendMessageButton {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.type = "button";
+    this.color = "green";
+  }
+  onClick() {
+    console.log(
+      `this.props: ${this.width}, ${this.height}, ${this.type}, ${this.color}`
+    );
+  }
+}
+
+let newWidth = 10;
+let newHeight = 5;
+
+let newButton = new sendMessageButton(newWidth, newHeight);
+
+console.log(newButton.onClick());
+
+function testButton() {
+  if (
+    ((newButton.width = newWidth),
+    (newButton.Height = newHeight),
+    (newButton.type = "button"),
+    (newButton.color = "green"))
+  ) {
+    console.log("Соответствует");
+  } else {
+    console.log("Не соответствует");
+  }
+}
+
+testButton();
