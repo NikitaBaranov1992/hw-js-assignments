8db00c03639e9e57ad54ca3a1023e1d9e719d00a
NikitaBaranov1992 <2486010@mail.ru>
2023-04-24 22:36:25 +0500

    homework 7.1.3

--- homework 7.1.js
+++ homework 7.1.js
@@ -1,6 +1,10 @@
 const postMatList = [null, null, null, "A001DFX0", null];
 
 let summAcc = 100;
+let summParcel = 100;
+
+summAcc -= summParcel;
+
 let codeMessage = "A001DFX0";
 
 for (let parcel of postMatList) {
@@ -8,5 +12,7 @@ for (let parcel of postMatList) {
     console.log(
       `Вы получили посылку из ячейки 4 и ваш счет составляет: ${summAcc} р.`
     );
+  } else {
+    console.log(`Введен неверный код, обратитесь в службу поддержки`);
   }
 }
